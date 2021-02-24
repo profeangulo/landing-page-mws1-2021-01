cf9784f2170be4492fd3827ccacc504216c408dd
Alberto Angulo <gerencia@angulodigital.co>
2021-02-24 16:03:41 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-/* JS*/
+// Activador de jQuery - Función principal de jQuery
+$(document).ready(function(){
+
+    // Activación de transición del menú principal y del menú móvil
+    $('header article nav a, aside#menu-moviles nav a').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000,'easeOutExpo');
+        event.preventDefault();
+    });
+
+});
